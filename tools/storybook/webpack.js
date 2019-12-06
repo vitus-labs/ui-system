b57e5f1fdef9bac7b3246c3a3473a768bded67ca
Vit Bokisch <vit@bokisch.cz>
2019-12-06 18:03:58 +0100

    update storybook

--- tools/storybook/webpack.js
+++ tools/storybook/webpack.js
@@ -25,12 +25,12 @@ module.exports = ({ config }) => {
   //   test: /\.(ts|tsx)$/,
   //   use: [
   //     {
-  //       loader: require.resolve('awesome-typescript-loader')
-  //     },
-  //     // Optional
-  //     {
-  //       loader: require.resolve('react-docgen-typescript-loader')
+  //       //       loader: require.resolve('awesome-typescript-loader')
   //     }
+  //     //     // Optional
+  //     //     {
+  //     //       loader: require.resolve('react-docgen-typescript-loader')
+  //     //     }
   //   ]
   // })
   // config.resolve.extensions.push('.ts', '.tsx')
