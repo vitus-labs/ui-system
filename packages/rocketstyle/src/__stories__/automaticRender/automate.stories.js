c940bb40de72315c58f57f032d1ba04151882b7c
Vit Bokisch <vit@bokisch.cz>
2020-03-20 23:36:45 +0100

    rocketstyle: update automated stories

--- packages/rocketstyle/src/__stories__/automaticRender/automate.stories.js
+++ packages/rocketstyle/src/__stories__/automaticRender/automate.stories.js
@@ -1,34 +1,37 @@
-import { boolean, text } from '@storybook/addon-knobs'
 import theme from '../bootstrap-4/themeDecorator'
 import Button from '../bootstrap-4/Button'
-import RocketStyleRender from '../../generateStories'
+import RocketStyleDocs from '../../generateStories'
 
 storiesOf('ROCKETSTYLE | Automatic', module)
   .addDecorator(theme)
   .add('Button - general', () => {
     return (
-      <RocketStyleRender
+      <RocketStyleDocs
         component={Button}
         type="documentation"
         props={{
-          onClick: () => {},
-          label: text('Name', 'Arunoda Susiripala')
+          onClick: () => {
+            console.log('hello')
+          },
+          label: 'Arunoda Susiripala'
         }}
       />
     )
   })
   .add('Button - states', () => {
     const props = ({ state }) => ({
-      onClick: () => {},
+      onClick: () => {
+        console.log('docs')
+      },
       label: `${state} button`
     })
-    return <RocketStyleRender component={Button} type="states" props={props} />
+    return <RocketStyleDocs component={Button} type="states" props={props} />
   })
   .add('Button - sizes', () => {
     const props = ({ size }) => ({
       onClick: () => {},
       label: `${size} button`
     })
-    return <RocketStyleRender component={Button} type="sizes" props={props} />
+    return <RocketStyleDocs component={Button} type="sizes" props={props} />
   })
   .add('Hello', () => <Button multiple={['outline']}>Hello</Button>)
