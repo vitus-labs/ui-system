01a3a9ba416e93cb159f98be7ffb1314f8586907
Vit Bokisch <vit@bokisch.cz>
2020-04-19 00:29:05 +0200

    refactor rollup config

--- packages/coolgrid/rollup.config.js
+++ packages/coolgrid/rollup.config.js
@@ -1,11 +1,3 @@
 import rollup from '@vitus-labs/tools-rollup'
 
-export default rollup({
-  name: 'coolgrid',
-  external: ['react', '@vitus-labs/core', '@vitus-labs/unistyle'],
-  globals: {
-    react: 'React',
-    '@vitus-labs/core': '@vitus-labs/core',
-    '@vitus-labs/unistyle': '@vitus-labs/unistyle',
-  },
-})
+export default rollup
--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,11 +1,3 @@
 import rollup from '@vitus-labs/tools-rollup'
 
-export default rollup({
-  name: 'core',
-  external: ['react', 'styled-components', 'lodash'],
-  globals: {
-    react: 'React',
-    'styled-components': 'styled',
-    lodash: 'lodash',
-  },
-})
+export default rollup
