3e38acf78b59543f4354ee91647c19b69b22dc04
Vit Bokisch <vit@bokisch.cz>
2020-05-28 20:25:53 +0200

    add refactored hoc

---
+++ packages/elements/src/Element/withEqualWidthBeforeAfter.tsx
@@ -0,0 +1,79 @@
+import React, { useEffect, createRef, ReactNode } from 'react'
+import { get } from '@vitus-labs/core'
+
+const isNumber = (a: any, b: any) => Number.isInteger(a) && Number.isInteger(b)
+
+const types = {
+  height: 'offsetHeight',
+  width: 'offsetWidth',
+}
+
+const calculate = ({ beforeContent, afterContent }) => (
+  type: keyof typeof types
+) => {
+  const beforeContentSize = get(beforeContent, types[type])
+  const afterContentSize = get(afterContent, types[type])
+
+  if (isNumber(beforeContentSize, afterContentSize)) {
+    if (beforeContentSize > afterContentSize) {
+      beforeContent.style[type] = `${beforeContentSize}px`
+      afterContent.style[type] = `${beforeContentSize}px`
+    } else {
+      beforeContent.style[type] = `${afterContentSize}px`
+      afterContent.style[type] = `${afterContentSize}px`
+    }
+  }
+}
+
+interface Props {
+  equalBeforeAfter: boolean
+}
+
+const withEqualBeforeAfter = <T extends Partial<Props>>(
+  WrappedComponent: React.ComponentType<T>
+): {
+  (props: T): JSX.Element
+  displayName: string
+} => {
+  const displayName =
+    WrappedComponent.displayName || WrappedComponent.name || 'Component'
+
+  const Enhanced = (props: Omit<T, keyof Props>) => {
+    const {
+      equalBeforeAfter,
+      vertical,
+      afterContent,
+      beforeContent,
+      ...rest
+    } = props
+    const elementRef = createRef()
+
+    const calculateSize = () => {
+      const beforeContent = get(elementRef, 'current.children[0]')
+      const afterContent = get(elementRef, 'current.children[2]')
+      const updateElement = calculate({ beforeContent, afterContent })
+
+      if (vertical) updateElement('height')
+      else updateElement('width')
+    }
+
+    useEffect(() => {
+      calculateSize()
+    }, [equalBeforeAfter, beforeContent, afterContent])
+
+    return (
+      <WrappedComponent
+        {...(rest as T)}
+        afterContent={afterContent}
+        beforeContent={beforeContent}
+        ref={elementRef}
+      />
+    )
+  }
+
+  Enhanced.displayName = displayName
+
+  return Enhanced
+}
+
+export default withEqualBeforeAfter
