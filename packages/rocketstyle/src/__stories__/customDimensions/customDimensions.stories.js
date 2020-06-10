c48db73c39cc0c1bdaefb3d08056e35c6142bc98
Vit Bokisch <vit@bokisch.cz>
2020-06-10 21:02:21 +0200

    fix custom dimensions configuration (#15)

---
+++ packages/rocketstyle/src/__stories__/customDimensions/customDimensions.stories.js
@@ -0,0 +1,10 @@
+// import Button, { HoistedButton, ElementExample } from '../Button'
+import Button from './component'
+
+storiesOf('ROCKETSTYLE | Element', module).add('Custom dimensions', () => {
+  return (
+    <>
+      <Button gapxl />
+    </>
+  )
+})
