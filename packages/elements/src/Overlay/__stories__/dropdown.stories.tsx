0761597fdf727104d7941f7b9de421f7a382ba2a
Vit Bokisch <vit@bokisch.cz>
2020-06-13 00:07:29 +0200

    [Elements] - Overlay, Portal, Util component rewrite, add types (#17)
    * rewrite Overlay component
    
    * Refactor portal
    
    * Overlay - fix bugs, improve performance
    
    * Overlay: fix calculation

---
+++ packages/elements/src/Overlay/__stories__/dropdown.stories.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { config } from '@vitus-labs/core'
+import Overlay from '..'
+
+const Trigger = ({ innerRef, ...props }: any) => (
+  <button ref={innerRef} {...props}>
+    Click on me
+  </button>
+)
+
+const Menu = ({ innerRef, ...props }: any) => (
+  <button ref={innerRef} {...props}>
+    some content here
+  </button>
+)
+
+storiesOf('ELEMENTS | Overlay', module).add('Dropdown', () => {
+  return (
+    <>
+      <Overlay refName="innerRef" trigger={(props) => <Trigger {...props} />}>
+        <Menu />
+      </Overlay>
+      <Overlay refName="innerRef" trigger={(props) => <Trigger {...props} />}>
+        <Menu />
+      </Overlay>
+      <Overlay refName="innerRef" trigger={(props) => <Trigger {...props} />}>
+        <Menu />
+      </Overlay>
+      <div style={{ position: 'absolute', right: 40 }}>
+        <Overlay refName="innerRef" trigger={(props) => <Trigger {...props} />}>
+          <Menu />
+        </Overlay>
+      </div>
+    </>
+  )
+})
--- packages/elements/src/Portal/component.ts
+++ packages/elements/src/Portal/component.ts
@@ -1,21 +1,25 @@
-import { Component } from 'react'
+import { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
-export default class Portal extends Component {
-  static displayName = 'vitus-labs/elements/Portal'
-
-  element = document.createElement('div')
-
-  componentDidMount() {
-    document.body.appendChild(this.element)
-  }
-
-  componentWillUnmount() {
-    document.body.removeChild(this.element)
-  }
-
-  render() {
-    const { children } = this.props
-    return ReactDOM.createPortal(children, this.element)
-  }
+interface Props {
+  children: React.ReactNode
+  tag?: string
 }
+
+const component = ({ tag = 'div', children }: Props) => {
+  const [element] = useState(document.createElement(tag))
+
+  useEffect(() => {
+    document.body.appendChild(element)
+
+    return () => {
+      document.body.removeChild(element)
+    }
+  }, [])
+
+  return ReactDOM.createPortal(children, element)
+}
+
+component.displayName = 'vitus-labs/elements/Portal'
+
+export default component
