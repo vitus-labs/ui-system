0c38dfafb45d5557cff73f26eec22333771fbbe7
Vit Bokisch <vit@bokisch.cz>
2020-07-01 20:17:58 +0200

    add some performance improvements

--- packages/rocketstyle/src/hooks/useTheme.tsx
+++ packages/rocketstyle/src/hooks/useTheme.tsx
@@ -1,5 +1,4 @@
 // @ts-nocheck
-import { useContext, useEffect } from 'react'
 import { config } from '@vitus-labs/core'
 import { calculateChainOptions } from '../utils'
 
@@ -18,55 +17,41 @@ const generateThemes = (context, theme, options) => {
   })
 }
 
-const useTheme = ({ options, onMount }) => {
-  const theme = useContext(config.context)
-
+const useTheme = ({ theme, options }) => {
   // define empty objects so they can be reassigned later
   const __ROCKETSTYLE__ = {
     keys: {},
     themes: {},
   }
 
-  const calculate = () => {
-    __ROCKETSTYLE__.themes.base = calculateChainOptions(
-      options.theme,
-      theme,
-      config.css
-    )
+  __ROCKETSTYLE__.themes.base = calculateChainOptions(
+    options.theme,
+    theme,
+    config.css
+  )
 
-    generateThemes(__ROCKETSTYLE__.themes, theme, options)
-    generateKeys(__ROCKETSTYLE__.keys, __ROCKETSTYLE__.themes, options)
+  generateThemes(__ROCKETSTYLE__.themes, theme, options)
+  generateKeys(__ROCKETSTYLE__.keys, __ROCKETSTYLE__.themes, options)
 
-    __ROCKETSTYLE__.rocketConfig = {}
-    __ROCKETSTYLE__.rocketConfig.dimensions = options.dimensions
-    __ROCKETSTYLE__.rocketConfig.useBooleans = options.useBooleans
+  __ROCKETSTYLE__.rocketConfig = {}
+  __ROCKETSTYLE__.rocketConfig.dimensions = options.dimensions
+  __ROCKETSTYLE__.rocketConfig.useBooleans = options.useBooleans
 
-    __ROCKETSTYLE__.KEYWORDS = []
+  __ROCKETSTYLE__.KEYWORDS = []
 
-    options.dimensionKeys.forEach((item) => {
-      __ROCKETSTYLE__.KEYWORDS = [
-        ...__ROCKETSTYLE__.KEYWORDS,
-        ...__ROCKETSTYLE__.keys[item],
-      ]
-    })
-
+  options.dimensionKeys.forEach((item) => {
     __ROCKETSTYLE__.KEYWORDS = [
       ...__ROCKETSTYLE__.KEYWORDS,
-      ...options.dimensionValues,
+      ...__ROCKETSTYLE__.keys[item],
     ]
-  }
+  })
 
-  calculate()
+  __ROCKETSTYLE__.KEYWORDS = [
+    ...__ROCKETSTYLE__.KEYWORDS,
+    ...options.dimensionValues,
+  ]
 
-  useEffect(() => {
-    calculate()
-
-    if (onMount) {
-      onMount(__ROCKETSTYLE__)
-    }
-  }, []) // Empty array ensures that effect is only run on mount and unmount
-
-  return { __ROCKETSTYLE__, theme }
+  return { __ROCKETSTYLE__ }
 }
 
 export default useTheme
