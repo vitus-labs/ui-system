d276b123f4dfbfb897d3e8996eb2035ddf52b0b5
Vit Bokisch <vit@bokisch.cz>
2020-09-01 08:52:01 +0200

    refactor stories

--- packages/elements/src/Element/__stories__/Alignment.stories.tsx
+++ packages/elements/src/Element/__stories__/Alignment.stories.tsx
@@ -1,133 +1,109 @@
 import React from 'react'
-import Element, { withEqualSizeBeforeAfter } from '~/Element'
+import Element from '~/Element'
 import { Wrapper, Inner } from './components'
 
-const NewElement = withEqualSizeBeforeAfter(Element)
+export default {
+  component: Element,
+  title: `${Element.displayName}/Inline Element`,
+}
 
-//   < NewElement
-// block
-// equalBeforeAfter
-// beforeContent = { Inner }
-// afterContent = { Inner }
-//   >
-//   <Inner />
-//       </NewElement >
-
-storiesOf('ELEMENTS | Element', module).add('Alignment', () => {
-  return (
-    <Fragment>
-      {/* <Element block beforeContent={Inner} afterContent={Inner} /> */}
-      {/* <NewElement
-        beforeContent={
-          <>
-            <Inner />
-            <Inner />
-            <Inner />
-            <Inner />
-            <Inner />
-            <Inner />
-          </>
-        }
-        afterContent={Inner}
-      >
-        some content
-      </NewElement> */}
-      <Wrapper block={false} beforeContent={Inner} afterContent={Inner}>
-        <Inner />
-      </Wrapper>
-      <Wrapper block beforeContent={Inner} afterContent={Inner}>
-        <Inner />
-      </Wrapper>
-      <Wrapper
-        block
-        beforeContent={Inner}
-        afterContent={Inner}
-        beforeContentAlignY="top"
-        contentAlignY="top"
-        afterContentAlignY="top"
-      >
-        <Inner />
-      </Wrapper>
-      <Wrapper
-        block
-        beforeContent={Inner}
-        afterContent={Inner}
-        beforeContentAlignY="bottom"
-        contentAlignY="bottom"
-        afterContentAlignY="bottom"
-      >
-        <Inner />
-      </Wrapper>
-      <Wrapper
-        block
-        beforeContent={Inner}
-        afterContent={Inner}
-        beforeContentAlignY="top"
-        contentAlignY="center"
-        afterContentAlignY="bottom"
-      >
-        <Inner />
-      </Wrapper>
-      <Wrapper
-        block
-        beforeContent={Inner}
-        afterContent={Inner}
-        beforeContentAlignY="bottom"
-        contentAlignY="center"
-        afterContentAlignY="top"
-      >
-        <Inner />
-      </Wrapper>
-      <Wrapper
-        block
-        beforeContent={Inner}
-        afterContent={Inner}
-        beforeContentAlignX="left"
-        contentAlignX="left"
-        afterContentAlignX="left"
-      >
-        <Inner />
-      </Wrapper>
-      <Wrapper
-        block
-        beforeContent={Inner}
-        afterContent={Inner}
-        beforeContentAlignX="center"
-        contentAlignX="center"
-        afterContentAlignX="center"
-      >
-        <Inner />
-      </Wrapper>
-      <Wrapper
-        block
-        beforeContent={Inner}
-        afterContent={Inner}
-        beforeContentAlignX="right"
-        contentAlignX="right"
-        afterContentAlignX="right"
-      >
-        <Inner />
-      </Wrapper>
-      <Wrapper
-        block
-        beforeContent={Inner}
-        afterContent={Inner}
-        beforeContentAlignX="left"
-        contentAlignX="center"
-        afterContentAlignX="right"
-      >
-        <Inner />
-      </Wrapper>
-      <Wrapper
-        block
-        beforeContent={Inner}
-        afterContent={Inner}
-        beforeContentAlignX="right"
-        contentAlignX="center"
-        afterContentAlignX="left"
-      >
-        <Inner />
-      </Wrapper>
-    </Fragment>
-  )
-})
+export const inlineElement = () => (
+  <>
+    <Wrapper block={false} beforeContent={Inner} afterContent={Inner}>
+      <Inner />
+    </Wrapper>
+    <Wrapper block beforeContent={Inner} afterContent={Inner}>
+      <Inner />
+    </Wrapper>
+    <Wrapper
+      block
+      beforeContent={Inner}
+      afterContent={Inner}
+      beforeContentAlignY="top"
+      contentAlignY="top"
+      afterContentAlignY="top"
+    >
+      <Inner />
+    </Wrapper>
+    <Wrapper
+      block
+      beforeContent={Inner}
+      afterContent={Inner}
+      beforeContentAlignY="bottom"
+      contentAlignY="bottom"
+      afterContentAlignY="bottom"
+    >
+      <Inner />
+    </Wrapper>
+    <Wrapper
+      block
+      beforeContent={Inner}
+      afterContent={Inner}
+      beforeContentAlignY="top"
+      contentAlignY="center"
+      afterContentAlignY="bottom"
+    >
+      <Inner />
+    </Wrapper>
+    <Wrapper
+      block
+      beforeContent={Inner}
+      afterContent={Inner}
+      beforeContentAlignY="bottom"
+      contentAlignY="center"
+      afterContentAlignY="top"
+    >
+      <Inner />
+    </Wrapper>
+    <Wrapper
+      block
+      beforeContent={Inner}
+      afterContent={Inner}
+      beforeContentAlignX="left"
+      contentAlignX="left"
+      afterContentAlignX="left"
+    >
+      <Inner />
+    </Wrapper>
+    <Wrapper
+      block
+      beforeContent={Inner}
+      afterContent={Inner}
+      beforeContentAlignX="center"
+      contentAlignX="center"
+      afterContentAlignX="center"
+    >
+      <Inner />
+    </Wrapper>
+    <Wrapper
+      block
+      beforeContent={Inner}
+      afterContent={Inner}
+      beforeContentAlignX="right"
+      contentAlignX="right"
+      afterContentAlignX="right"
+    >
+      <Inner />
+    </Wrapper>
+    <Wrapper
+      block
+      beforeContent={Inner}
+      afterContent={Inner}
+      beforeContentAlignX="left"
+      contentAlignX="center"
+      afterContentAlignX="right"
+    >
+      <Inner />
+    </Wrapper>
+    <Wrapper
+      block
+      beforeContent={Inner}
+      afterContent={Inner}
+      beforeContentAlignX="right"
+      contentAlignX="center"
+      afterContentAlignX="left"
+    >
+      <Inner />
+    </Wrapper>
+  </>
+)
--- packages/elements/src/Element/__stories__/Base.stories.tsx
+++ packages/elements/src/Element/__stories__/Base.stories.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { config } from '@vitus-labs/core'
 import Element from '~/Element'
 
+export default {
+  component: Element,
+  title: `${Element.displayName}/Single Elements`,
+}
+
 const extendCss = config.css`
   background-color: #fff;
   border: 1px solid black;
@@ -20,43 +25,33 @@ const beforeContent = ({ hover, ...props }) => {
   return <span>{hover ? 'normal' : 'hover'}</span>
 }
 
-storiesOf('ELEMENTS | Element', module)
-  .add('Basics', () => {
-    return (
-      <Element
-        css={extendCss}
-        beforeContent={beforeContent}
-        afterContent="right"
-      >
-        child
-      </Element>
-    )
-  })
-  .add('Empty element (e.g. input)', () => {
-    return (
-      <Element tag="input" css={extendCss}>
-        child
-      </Element>
-    )
-  })
+export const Basics = () => (
+  <Element css={extendCss} beforeContent={beforeContent} afterContent="right">
+    child
+  </Element>
+)
 
-  .add('Dangerously set inner content', () => {
-    function createMarkup() {
-      return {
-        __html: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+export const emptyElement = () => (
+  <Element tag="input" css={extendCss}>
+    child
+  </Element>
+)
+
+export const dangerouslySetinner = () => {
+  const createMarkup = () => ({
+    __html: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M8.43895 4.93599C7.85368 5.52125 7.85368 6.47285 8.43895 7.05811L13.4252 12.0397L8.89355 16.9821C8.33141 17.5967 8.37776 18.5412 8.98279 19.1014L8.98505 19.1035C9.59965 19.6656 10.5442 19.6193 11.1044 19.0143L16.6061 13.0124L16.607 13.0114C17.1451 12.421 17.132 11.5069 16.5611 10.936L10.5611 4.93599C9.97579 4.35073 9.02421 4.35073 8.43895 4.93599Z" fill="currentColor"/>
 </svg>
 `,
-      }
-    }
-
-    return (
-      <Element
-        tag="span"
-        css={extendCss}
-        dangerouslySetInnerHTML={createMarkup()}
-      >
-        child
-      </Element>
-    )
   })
+
+  return (
+    <Element
+      tag="span"
+      css={extendCss}
+      dangerouslySetInnerHTML={createMarkup()}
+    >
+      child
+    </Element>
+  )
+}
