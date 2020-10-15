da312acbf390a4a624036ed71f24d785af010071
Vit Bokisch <vit@bokisch.cz>
2020-10-15 14:11:43 +0200

    clean storybook typescript

--- tools/storybook/config/main.js
+++ tools/storybook/config/main.js
@@ -2,8 +2,8 @@ const path = require('path')
 
 module.exports = {
   stories: [
-    `${process.cwd()}/src/**/*.stories.(js|jsx|ts|tsx|mdx)`,
-    `${process.cwd()}/src/**/stories.(js|jsx|ts|tsx|mdx)`,
+    `${process.cwd()}/src/**/*.stories.@(js|jsx|ts|tsx|mdx)`,
+    `${process.cwd()}/src/**/stories.@(js|jsx|ts|tsx|mdx)`,
   ],
   addons: [
     '@storybook/addon-viewport/register',
@@ -14,29 +14,8 @@ module.exports = {
   ],
 
   webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      loader: require.resolve('babel-loader'),
-    })
-    config.resolve.extensions.push('.ts', '.tsx')
     config.resolve.alias['~'] = path.resolve(process.cwd(), 'src/')
 
     return config
-  },
-
-  // webpackFinal: async (config) => {
-  //   config.module.rules.push({
-  //     test: /\.(ts|tsx)$/,
-  //     use: [
-  //       {
-  //         loader: require.resolve('ts-loader'),
-  //       },
-  //       {
-  //         loader: require.resolve('react-docgen-typescript-loader'),
-  //       },
-  //     ],
-  //   })
-  //   config.resolve.extensions.push('.ts', '.tsx')
-  //   return config
-  // },
+  }
 }
