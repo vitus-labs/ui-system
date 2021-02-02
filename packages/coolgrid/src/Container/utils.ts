6801769592a7a05992184ba55f2635952fd46571
Vit Bokisch <vit@bokisch.cz>
2021-02-02 15:35:26 +0100

    coolgrid: fix linter

--- packages/coolgrid/src/Container/utils.ts
+++ packages/coolgrid/src/Container/utils.ts
@@ -4,7 +4,12 @@ import { get } from '@vitus-labs/core'
 // ------------------------------------------
 // get container width
 // ------------------------------------------
-export const getContainerWidth = (props = {}, theme = {}) =>
+type GetContainerWidth = (
+  props?: Record<string, unknown> | unknown,
+  theme?: Record<string, unknown> | unknown
+) => Record<string, unknown>
+
+export const getContainerWidth: GetContainerWidth = (props, theme) =>
   get(props, 'width') ||
   get(theme, 'grid.container') ||
   get(theme, 'coolgrid.container')
