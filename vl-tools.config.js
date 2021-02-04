6933e9e23eb1023087e6d78d48d98c6467af95c4
Vit Bokisch <vit@bokisch.cz>
2021-02-04 14:55:31 +0100

    pkgs update

--- vl-tools.config.js
+++ vl-tools.config.js
@@ -11,25 +11,26 @@ module.exports = {
     },
   },
   stories: {
-    port: 7007,
     font: 'https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap',
-    theme: {
-      rootSize: 16,
-      breakpoints: {
-        xs: 0,
-        sm: 576,
-        md: 768,
-        lg: 992,
-        xl: 1200,
-      },
-      grid: {
-        columns: 12,
-        container: {
-          xs: '100%',
-          sm: 540,
-          md: 720,
-          lg: 960,
-          xl: 1140,
+    decorators: {
+      theme: {
+        rootSize: 16,
+        breakpoints: {
+          xs: 0,
+          sm: 576,
+          md: 768,
+          lg: 992,
+          xl: 1200,
+        },
+        grid: {
+          columns: 12,
+          container: {
+            xs: '100%',
+            sm: 540,
+            md: 720,
+            lg: 960,
+            xl: 1140,
+          },
         },
       },
     },
