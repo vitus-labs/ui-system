7dd29a747b435b66f4b570d89388b840c7f47eb2
Vit Bokisch <vit@bokisch.cz>
2021-03-25 12:29:16 +0100

    fix types

--- packages/unistyle/src/styles/extendCss.ts
+++ packages/unistyle/src/styles/extendCss.ts
@@ -1,8 +1,12 @@
 import { config } from '@vitus-labs/core'
 
 export type ExtendCss = (
-  styles: string | ((css: typeof config.css) => typeof css) | null | undefined
-) => string | typeof config.css
+  styles:
+    | string
+    | ((css: typeof config.css) => ReturnType<typeof css>)
+    | null
+    | undefined
+) => string | ReturnType<typeof config.css>
 
 const extendCss: ExtendCss = (styles) => {
   if (!styles) return ''
