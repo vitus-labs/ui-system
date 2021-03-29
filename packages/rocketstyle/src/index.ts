7d877f16537c524d7caadb06114ce669ac79d4aa
Vit Bokisch <vit@bokisch.cz>
2021-03-29 16:20:43 +0200

    rocketstyle: add isRocketstyle helper

--- packages/rocketstyle/src/index.ts
+++ packages/rocketstyle/src/index.ts
@@ -1,7 +1,8 @@
 import rocketstyle, { Rocketstyle } from './init'
 import Provider, { context, TProvider } from './context'
+import isRocketComponent from './isRocketComponent'
 
 export type { Rocketstyle, TProvider }
 
-export { context, Provider }
+export { context, Provider, isRocketComponent }
 export default rocketstyle
