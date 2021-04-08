286be2c390cdf262cdf7398f1661b6d7bce09216
Vit Bokisch <vit@bokisch.cz>
2021-04-08 13:35:10 +0200

    [Elements] Add forwardRef and tag types (#48)
    * [Elements] Text add forwardRef
    
    * [Elements] add tags types

--- packages/elements/src/Text/component.tsx
+++ packages/elements/src/Text/component.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React from 'react'
-import type { ReactNode } from 'react'
-import type { StyledComponentPropsWithRef } from 'styled-components'
+import React, { forwardRef, ReactNode, ForwardRefExoticComponent } from 'react'
+import type { HTMLTags } from '@vitus-labs/core'
 import Styled from './styled'
 import type { ExtendCss } from '~/types'
 
@@ -9,29 +8,26 @@ export type Props = Partial<{
   paragraph: boolean
   label: ReactNode
   children: ReactNode
-  tag: StyledComponentPropsWithRef<any>
+  tag: HTMLTags
   extendCss: ExtendCss
 }>
 
-const Element: React.FC<Props> & { isText: boolean } = ({
-  paragraph,
-  label,
-  children,
-  tag,
-  extendCss,
-  ...props
-}) => {
-  const renderContent = (as = undefined) => (
-    <Styled as={as} $text={{ extraStyles: extendCss }} {...props}>
-      {children || label}
-    </Styled>
-  )
+const Element: ForwardRefExoticComponent<Props> & {
+  isText?: true
+} = forwardRef<any, Props>(
+  ({ paragraph, label, children, tag, extendCss, ...props }, ref) => {
+    const renderContent = (as = undefined) => (
+      <Styled ref={ref} as={as} $text={{ extraStyles: extendCss }} {...props}>
+        {children || label}
+      </Styled>
+    )
 
-  // eslint-disable-next-line no-nested-ternary
-  const finalTag = __WEB__ ? (paragraph ? 'p' : tag) : undefined
+    // eslint-disable-next-line no-nested-ternary
+    const finalTag = __WEB__ ? (paragraph ? 'p' : tag) : undefined
 
-  return renderContent(finalTag)
-}
+    return renderContent(finalTag)
+  }
+)
 
 Element.displayName = 'vitus-labs/elements/Text'
 Element.isText = true
